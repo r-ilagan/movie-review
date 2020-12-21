90f3323fadda6d7000b6cd6cd8c9c8df361a7174
Rach Ilagan <cachola.matthew@gmail.com>
2020-12-21 04:21:31 -0500

    feat: Show individual movie data into a component

---
+++ src/components/Movie.js
@@ -0,0 +1,12 @@
+const Movie = ({ movie }) => {
+  return (
+    <article>
+      <img src={movie.Poster} alt="movie poster" />
+      <h3>{movie.Title}</h3>
+      <p>Year {movie.Year}</p>
+      <a href={`/movies/${movie.imdbID}`}>More</a>
+    </article>
+  );
+};
+
+export default Movie;
