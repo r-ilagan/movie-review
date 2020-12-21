1dcc323eb2d5a8875ea20170ffb277228f36e72f
Rach Ilagan <cachola.matthew@gmail.com>
2020-12-21 04:21:12 -0500

    feat: Include Movies component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import SearchBar from './components/SearchBar';
+import Movies from './components/Movies';
 
 function App() {
   const [query, setQuery] = useState('');
@@ -8,6 +9,7 @@ function App() {
   return (
     <div>
       <SearchBar query={query} setQuery={setQuery} setMovies={setMovies} />
+      <Movies movies={movies} />
     </div>
   );
 }
