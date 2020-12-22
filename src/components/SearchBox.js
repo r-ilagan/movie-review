96a0f83ea6fffe0949b6a5edb4312951ca7e49e1
Rach Ilagan <cachola.matthew@gmail.com>
2020-12-22 02:55:01 -0500

    feat: Add a way to change url after search

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,10 +1,14 @@
 import { searchForMovies } from '../services/movie';
+import { useHistory } from 'react-router-dom';
 
 const SearchBox = ({ query, setQuery, setMovies }) => {
+  const history = useHistory();
+
   const findMovies = async (e) => {
     e.preventDefault();
     await searchForMovies(query)
       .then((data) => setMovies(data.Search))
+      .then(() => history.push(`/s/${query}`))
       .catch((error) => console.log(error));
   };
 
