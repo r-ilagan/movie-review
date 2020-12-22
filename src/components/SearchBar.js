50561d7d86152abbba56922c5fddba500cd20c3d
Rach Ilagan <cachola.matthew@gmail.com>
2020-12-22 00:31:32 -0500

    refactor: Remove h2 tag

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,6 @@
 import { searchForMovies } from '../services/movie';
 
-function SearchBar({ query, setQuery, setMovies }) {
+const SearchBar = ({ query, setQuery, setMovies }) => {
   const findMovies = async () => {
     await searchForMovies(query)
       .then((data) => setMovies(data.Search))
@@ -13,11 +13,10 @@ function SearchBar({ query, setQuery, setMovies }) {
 
   return (
     <section>
-      <h2>Search for a movie</h2>
       <input type="text" onChange={onChange} />
       <button onClick={findMovies}>Search</button>
     </section>
   );
-}
+};
 
 export default SearchBar;
