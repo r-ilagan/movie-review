28c066750f210e6bde534775562fefeced2fd35f
Rach Ilagan <cachola.matthew@gmail.com>
2020-12-22 00:32:18 -0500

    feat: Add the type of show

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,7 +3,8 @@ const Movie = ({ movie }) => {
     <article>
       <img src={movie.Poster} alt="movie poster" />
       <h3>{movie.Title}</h3>
-      <p>Year {movie.Year}</p>
+      <p>Year: {movie.Year}</p>
+      <p>Type: {movie.Type}</p>
       <a href={`/movies/${movie.imdbID}`}>More</a>
     </article>
   );
