bdd8dd5895736172e7e0e146cf71ceaccf15b944
Rach Ilagan <cachola.matthew@gmail.com>
2020-12-22 00:30:30 -0500

    feat: Implement basic routing with react-router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import { useState } from 'react';
-import SearchBar from './components/SearchBar';
+import About from './components/About';
+import Home from './components/Home';
 import Movies from './components/Movies';
+import NavBar from './components/NavBar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   const [query, setQuery] = useState('');
   const [movies, setMovies] = useState([]);
 
+  console.log(movies);
+
   return (
-    <div>
-      <SearchBar query={query} setQuery={setQuery} setMovies={setMovies} />
-      <Movies movies={movies} />
-    </div>
+    <Router>
+      <div>
+        <NavBar query={query} setQuery={setQuery} setMovies={setMovies} />
+        <Switch>
+          <Route exact path="/about" component={About} />
+          <Route exact path="/" component={Home} />
+        </Switch>
+        <Movies movies={movies} />
+      </div>
+    </Router>
   );
 }
 
