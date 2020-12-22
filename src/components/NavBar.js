0645787a21b110519c34cccdb238695ea378b935
Rach Ilagan <cachola.matthew@gmail.com>
2020-12-22 17:04:21 -0500

    feat: Add basic styling to nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,21 +5,27 @@ import { Link } from 'react-router-dom';
 
 const NavBar = ({ query, setQuery, setMovies }) => {
   return (
-    <nav>
-      <ul>
-        <Link to="/">
+    <header>
+      <nav>
+        <ul className="flex px-2 py-3 items-center justify-between">
+          <Link to="/">
+            <li>
+              <Logo height={20} />
+            </li>
+          </Link>
           <li>
-            <Logo />
+            <SearchBox
+              query={query}
+              setQuery={setQuery}
+              setMovies={setMovies}
+            />
           </li>
-        </Link>
-        <li>
-          <SearchBox query={query} setQuery={setQuery} setMovies={setMovies} />
-        </li>
-        <Link to="/about">
-          <li>About</li>
-        </Link>
-      </ul>
-    </nav>
+          <Link to="/about">
+            <li className="px-2">About</li>
+          </Link>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
