8db02e9766dcaf9a90f5bf4fad674680708ae20f
Rach Ilagan <cachola.matthew@gmail.com>
2020-12-24 02:10:21 -0500

    feat: Add a response to no result searches

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,17 @@
 import Movie from './Movie';
+import { useParams } from 'react-router-dom';
 
-const Movies = ({ movies }) => {
-  if (typeof movies === 'undefined' || movies.length === 0) {
-    return null;
+const Movies = ({ movies, response }) => {
+  const { query } = useParams();
+
+  if (response === 'FALSE') {
+    return (
+      <div className="text-center">
+        <p>
+          No results found for <span className="font-bold">"{query}"</span>
+        </p>
+      </div>
+    );
   }
 
   return (
