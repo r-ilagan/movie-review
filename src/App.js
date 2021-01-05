f6014d43aa8c7360f0220589042ac7bc8ad8dd8a
Rach Ilagan <cachola.matthew@gmail.com>
2021-01-04 20:28:41 -0500

    refactor: Remove Router in App

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import About from './components/About';
 import Home from './components/Home';
 import Movies from './components/Movies';
 import NavBar from './components/NavBar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   const [query, setQuery] = useState('');
@@ -11,23 +11,21 @@ function App() {
   const [response, setResponse] = useState('FALSE');
 
   return (
-    <Router>
-      <div>
-        <NavBar
-          query={query}
-          setQuery={setQuery}
-          setMovies={setMovies}
-          setResponse={setResponse}
-        />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/s/:query">
-            <Movies movies={movies} response={response} />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <div>
+      <NavBar
+        query={query}
+        setQuery={setQuery}
+        setMovies={setMovies}
+        setResponse={setResponse}
+      />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/s/:query">
+          <Movies movies={movies} response={response} />
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
