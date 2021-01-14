41769b88a52f6208162d0806707daba75c29e619
Rach Ilagan <cachola.matthew@gmail.com>
2021-01-14 15:26:34 -0500

    Add dark gray colour

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'my-gray': '#0D1117',
+      }
+    },
   },
   variants: {
     extend: {},
