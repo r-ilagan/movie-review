0db6dc14f0c95c070ca0b5bdf47dd0b3c14d9662
Rach Ilagan <cachola.matthew@gmail.com>
2021-01-13 20:19:32 -0500

    feat: Implement GET request to proxy

--- src/services/tmdb.js
+++ src/services/tmdb.js
@@ -1,5 +1,22 @@
 import axios from 'axios';
 
-axios.defaults.headers.common = {
-  Authorization: `Bearer ${process.env.REACT_APP_TMDB_TOKEN}`,
+const searchBaseUrl = 'http://localhost:3001/api/v1/tmdb';
+
+const SEARCH = {
+  ALL: `${searchBaseUrl}/search?`,
+};
+
+export const multiSearch = async (query, mode = 'multi', page = 1) => {
+  const params = new URLSearchParams({
+    mode,
+    query,
+    page,
+  });
+  try {
+    const response = await axios.get(`${SEARCH.ALL}${params}`);
+    console.log('at tmdb file', response.data);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
 };
