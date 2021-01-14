ac97b9e88b59c6b04940df7bb82aac6627ec49a8
Rach Ilagan <cachola.matthew@gmail.com>
2021-01-14 15:28:16 -0500

    feat: Add hook that returns if on mobile screen

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,5 +1,21 @@
+import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 export const useQuery = () => {
   return new URLSearchParams(useLocation().search);
+};
+
+export const useMobileSize = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+  function handleWindowSizeChange() {
+    setWidth(window.innerWidth);
+  }
+  useEffect(() => {
+    window.addEventListener('resize', handleWindowSizeChange);
+    return () => {
+      window.removeEventListener('resize', handleWindowSizeChange);
+    };
+  }, []);
+
+  return width <= 640;
 };
