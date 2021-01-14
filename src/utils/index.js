be763f64d05de71074a20bb9fae53c5ff8f4b78b
Rach Ilagan <cachola.matthew@gmail.com>
2021-01-14 15:27:41 -0500

    feat: Add util function for different image sizes

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,3 +2,10 @@ export const isObjEmpty = (obj) => {
   for (const i in obj) return false;
   return true;
 };
+
+export const getImageURL = (size, imagePath) => {
+  const SIZE = { SMALL: 'w92', MEDIUM: 'w154', LARGE: 'w342' };
+  const imageURL = `https://image.tmdb.org/t/p/${SIZE[size]}${imagePath}`;
+
+  return imageURL;
+};
