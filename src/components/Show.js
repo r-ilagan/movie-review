6a3f97e1bb9705882168b21bc344393c4bfedacb
Rach Ilagan <cachola.matthew@gmail.com>
2021-01-15 23:52:12 -0500

    refactor: Improve design and flow on mobile layout

--- src/components/Show.js
+++ src/components/Show.js
@@ -1,11 +1,31 @@
+import { useMobileSize } from '../hooks';
+import { getImageURL } from '../utils';
+import filmSVG from './icons/svg/film.svg';
+
 const Show = ({ show }) => {
+  const imageSize = useMobileSize() ? 'SMALL' : 'MEDIUM';
+  let imageURL;
+  if (!show.poster_path) {
+    imageURL = filmSVG;
+  } else {
+    imageURL = getImageURL(imageSize, show.poster_path);
+  }
+
   return (
-    <article>
-      <img src={show.poster} alt="movie poster" />
-      <h3>{show.Title}</h3>
-      <p>Year: {show.Year}</p>
-      <p>Type: {show.Type}</p>
-      <a href={`/show/${show.imdbID}`}>More</a>
+    <article className="flex">
+      <img src={imageURL} alt="show poster" />
+      <div className="flex flex-col py-3 px-2">
+        <h3 className="font-semibold text-lg">
+          <a href={`/show/${show.id}`}>
+            {show.title}{' '}
+            <span className="font-thin text-sm italic text-gray-300 text-opacity-40">
+              {show.release_date && new Date(show.release_date).getFullYear()}
+            </span>
+          </a>
+        </h3>
+
+        <p className="italic text-sm pt-1">{show.media_type}</p>
+      </div>
     </article>
   );
 };
